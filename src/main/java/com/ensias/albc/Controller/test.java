df5527445d18de9fb6f28753a50696460e4441f1
hiba-coder <hhiikkmmaa2@gmail.com>
2021-12-18 09:41:49 +0100

    first commit

---
+++ src/main/java/com/ensias/albc/Controller/test.java
@@ -0,0 +1,12 @@
+package com.ensias.albc.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+    @GetMapping("/test")
+    public String get() {
+        return "index";
+    }
+}
